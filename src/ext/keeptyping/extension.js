368e18379b79dd5dd0171419cfc6fb52842d64c8
Jiaman Wu <jwu7@ncsu.edu>
2017-04-10 02:38:57 -0400

    ext works but not light

--- src/ext/keeptyping/extension.js
+++ src/ext/keeptyping/extension.js
@@ -1,10 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
-//var timediff = require('timediff');
+//var blink = require('node-blink1');
 var date = require('date');
-
-const editor = vscode.window.activeTextEditor;
+var lastCodingTime = 0;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,21 +11,21 @@ function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "keeptyping" is now active!');
-    console.log(Date.now());
-    var lastCodingTime = 0;
-
-    while (vscode.window.onDidChangeActiveTextEditor){
-        lastCodingTime = Date.now();
-    }
-
+    
+vscode.workspace.onDidChangeTextDocument(e => editing());
+//one minute check once
     var check = setInterval(function(){
-        var diff = Date.now() - lastCodingTime;
-        if (diff > 1 * 60000){
-            console.log("Don't stop typing! ");
+        var now = Date.now();
+        var diff = now - lastCodingTime;
+        //console.log("now: " + now);
+        //console.log("lastCodingTime: " + lastCodingTime);
+        //console.log("diff: " + diff);
+        if (diff > 60 * 1000){
+            editOff();
         } else {
-            console.log("Keep typing! ");
+            editOn();
         }
-    }, 1 * 60000);
+    }, 60 * 1000);
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
@@ -49,9 +48,18 @@ function deactivate() {
 exports.deactivate = deactivate;
 
 function editOn() {
-    vscode.window.showInformationMessage('Keep typing!');
+    console.log("Keep typing! ");
+    //blink.fadeToRGB(1000, 0, 255, 0);
+    //vscode.window.showInformationMessage('Keep typing!');
 }
 
 function editOff() {
-    vscode.window.showInformationMessage('Don\'t stop typing!');
+    console.log("Don't stop typing! ");
+    //blink.fadeToRGB(1000, 255, 0, 0);
+    //vscode.window.showInformationMessage('Don\'t stop typing!');
+}
+
+function editing(){
+    lastCodingTime = Date.now();
+    //console.log(lastCodingTime);
 }
