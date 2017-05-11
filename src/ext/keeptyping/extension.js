3eed766b99adc60005477aeb4f32ebb1e706b7ff
Jiaman Wu <jwu7@ncsu.edu>
2017-05-11 16:02:45 -0400

    change activationEvent

--- src/ext/keeptyping/extension.js
+++ src/ext/keeptyping/extension.js
@@ -4,54 +4,24 @@ var vscode = require('vscode');
 var date = require('date');
 var request = require("request");
 var Promise = require('promise');
-
-var lastCodingTime = 0;
+//record the time of last editing
+var lastEditingTime = 0;
+//set Interval to check if have edited
+var check;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "keeptyping" is now active!');
-   
-    vscode.workspace.onDidChangeTextDocument(e => editing());
-    //one minute check once
-    var now = Date.now();
-    var diff = now - lastCodingTime;
-    if (diff > 10 * 1000){
-        editOff();
-    } else {
-        editOn();
-    }
-
-    var check = setInterval(function(){
-        var now = Date.now();
-        var diff = now - lastCodingTime;
-        //console.log("now: " + now);
-        //console.log("lastCodingTime: " + lastCodingTime);
-        //console.log("diff: " + diff);
-        if (diff > 10 * 1000){
-            editOff();
-        } else {
-            editOn();
-        }
-    }, 10 * 1000);
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    /*
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function (sessionLength) {
-        // The code you place here will be executed every time your command is executed
-       //editor.onDidChangeActiveTextEditor(editOn);
-       setTimeout(
-        function(){
-            clearInterval(check);
-        } , sessionLength * 60 * 1000);
- 
-    });
     
-    context.subscriptions.push(disposable);
-    */
+    var start = vscode.commands.registerCommand('extension.start', startFunc);
+    var close = vscode.commands.registerCommand('extension.close', closeFunc);
+
+    context.subscriptions.push(start);
+    context.subscriptions.push(close);
+ 
 }
 exports.activate = activate;
 
@@ -59,18 +29,41 @@ exports.activate = activate;
 function deactivate() {
     return new Promise(
         function(resolve, reject){
-            request.post('http://localhost:5000/Blink/off', function(err,res,body){
-                console.log("Closing! ");
-            });
+            closeFunc();
         }
     );
-    
 }
 exports.deactivate = deactivate;
 
+function startFunc() {
+    console.log('Congratulations, your extension "keeptyping" is now active!');
+   
+    vscode.workspace.onDidChangeTextDocument(e => editing());
+    //one minute check once
+    var now = Date.now();
+    var diff = now - lastEditingTime;
+    if (diff > 10 * 1000){
+        editOff();
+    } else {
+        editOn();
+    }
+
+    check = setInterval(function(){
+        var now = Date.now();
+        var diff = now - lastEditingTime;
+        //console.log("now: " + now);
+        //console.log("lastEditingTime: " + lastEditingTime);
+        //console.log("diff: " + diff);
+        if (diff > 10 * 1000){
+            editOff();
+        } else {
+            editOn();
+        }
+    }, 10 * 1000);
+}
+
 function editOn() {
     request.post('http://localhost:5000/Blink/editOn', function(err,res,body){
-        // callback
         console.log("Keep typing! ");
     }); 
 
@@ -78,12 +71,18 @@ function editOn() {
 
 function editOff() {
     request.post('http://localhost:5000/Blink/editOff', function(err,res,body){
-        // callback
         console.log("Don't stop typing! ");
     });
 }
 
-function editing(){
-    lastCodingTime = Date.now();
-    //console.log(lastCodingTime);
+function editing() {
+    lastEditingTime = Date.now();
+    //console.log(lastEditingTime);
+}
+
+function closeFunc() {
+    request.post('http://localhost:5000/Blink/off', function(err,res,body){
+                console.log("Closing! ");
+    });
+    clearInterval(check);
 }
